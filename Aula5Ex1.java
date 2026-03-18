fe3fb0ae5be6510870b221651fc7e5b4344dd232
kauecala <kaue.calasans@gmail.com>
2026-03-18 09:57:27 -0300

    Add files via upload

---
+++ Aula5Ex1.java
@@ -0,0 +1,11 @@
+public class Aula5Ex1 {
+    public static void main(String[] args) {
+        int contador; // declarando o contador
+        contador = 100; // inicializando o contador
+        while (contador <= 200) {
+            System.out.print(contador + " ");
+            contador++; // mesma coisa que utilizar contador = contador + 1
+        }
+
+    }
+}
